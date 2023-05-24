e2be86b750ccdf4b450d02dbe0914061b610d449
rathesh77 <rathesh.charles@outlook.fr>
2023-05-24 23:39:56 +0200

    node : add nginx folder creation on init.sh execution

--- src/node/init.sh
+++ src/node/init.sh
@@ -5,6 +5,21 @@
 
 sudo apt-get -y install nginx
 
+rm /etc/nginx/sites-enabled/default
+
+mkdir -p /etc/nginx/locations
+mkdir -p /etc/nginx/servers
+mkdir -p /etc/nginx/upstreams
+
+touch /etc/nginx/sites-available/custom.config.conf
+ln -s /etc/nginx/sites-available/custom.config.conf /etc/nginx/sites-enabled/custom.config.conf
+
+echo "error_log /var/log/nginx/error.log debug;
+include /etc/nginx/upstreams/*/*.conf;
+include /etc/nginx/servers/*/*.conf;" > /etc/nginx/sites-available/custom.config.conf
+
+systemctl restart nginx
+
 cd kubelet
 
 go run .
